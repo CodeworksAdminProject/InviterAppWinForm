c8c71599618c04a69fcd2443f4b6718208f79fe8
Глеб Михейчик <miheychik1997@mail.ru>
2018-02-22 09:10:42 +0300

    При нажатии на кнопку открывает другую страничку

---
+++ invAppMobail/invAppMobail/FirstPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace invAppMobail
+{
+	
+	public partial class FirstPage : ContentPage
+	{
+		public FirstPage ()
+		{
+			InitializeComponent ();
+		}
+        private void Back_Click(object sender, System.EventArgs e)
+        {
+            ToMainPage(sender, e);
+        }
+        public async void ToMainPage(object sender, EventArgs e)
+        {
+            await Navigation.PushModalAsync(new MainPage());
+        }
+        private void BarcodeRequest_Click(object sender, System.EventArgs e)
+        {
+            brr.Focus();
+            brr.Text = "Искать в базе (скан штрихкода)";
+        }
+    }
+}
--- invAppMobail/invAppMobail/MainPage.xaml.cs
+++ invAppMobail/invAppMobail/MainPage.xaml.cs
@@ -5,13 +5,47 @@ using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
 
+
+
 namespace invAppMobail
 {
 	public partial class MainPage : ContentPage
 	{
-		public MainPage()
+        
+        public MainPage()
 		{
 			InitializeComponent();
-		}
-	}
+        }
+        private void BarcodeRequest_Click(object sender, System.EventArgs e)
+        {
+            ToFirstPage(sender,e);
+
+        }
+        private async void ToFirstPage(object sender, EventArgs e)
+        {
+            await Navigation.PushModalAsync(new FirstPage());
+
+        }
+
+        private void NumberRequest_Click(object sender, System.EventArgs e)
+        {
+           // brr.Focus();
+          //  brr.Text = "Искать в базе (фильтр)";
+        }
+        private void Inventory_Click(object sender, System.EventArgs e)
+        {
+          //  brr.Focus();
+          //  brr.Text = "Инвентаризация";
+        }
+        private void Settings_Click(object sender, System.EventArgs e)
+        {
+          //  brr.Focus();
+          //  brr.Text = "Настройки";
+        }
+        private void Exit_Click(object sender, System.EventArgs e)
+        {
+          //  brr.Focus();
+          //  brr.Text = "Выход";
+        }
+    }
 }
